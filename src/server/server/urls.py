0a649415ded9676b2ba1d7cdcf358f6f7a9f3873
harvey.lee <harvey.lee@kakaocorp.com>
2020-08-01 14:12:18 +0900

    fix

--- src/server/server/urls.py
+++ src/server/server/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   # path('', include('accounts.urls')),
-    path('oauth/', include('allauth.urls')),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('weather/', include('weather.urls')),
     path('location/', include('location.urls')),
