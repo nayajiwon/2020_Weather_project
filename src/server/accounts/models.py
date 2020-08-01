83c77f944554386dda315aec454f5ac6c819bf09
harvey.lee <harvey.lee@kakaocorp.com>
2020-08-01 15:30:08 +0900

    fix

--- src/server/accounts/models.py
+++ src/server/accounts/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=True)
     pw = models.CharField(max_length=20)
-    is_active = models.BooleanField()
     kakao_id = models.IntegerField()
 
     def __str__(self):
