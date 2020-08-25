6b39a01cfaf7494b679793bd932c14e1e49edda6
Ubuntu <ubuntu@ip-172-31-44-94.us-east-2.compute.internal>
2020-08-25 17:00:53 +0900

    fix

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -17,7 +17,7 @@ import pymongo
 
 os.environ['API_KEY'] = '9a3bb4a52c09371ab3dcab7b20d03210'
 #os.environ['IP'] = '127.0.0.1'
-os.environ['IP'] = '52.26.131.225'
+os.environ['IP'] = '3.131.33.128'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
