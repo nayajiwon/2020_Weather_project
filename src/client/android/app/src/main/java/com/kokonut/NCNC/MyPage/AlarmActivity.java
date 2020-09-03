696cd343116254816e149be3d3d61872ba46ce12
nayajiwon <littlecherub@naver.com>
2020-09-03 14:11:12 +0900

    before final report

---
+++ src/client/android/app/src/main/java/com/kokonut/NCNC/MyPage/AlarmActivity.java
@@ -0,0 +1,18 @@
+package com.kokonut.NCNC.MyPage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kokonut.NCNC.R;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+
+    }
+}
---
+++ src/client/android/app/src/main/java/com/kokonut/NCNC/MyPage/MonAlarmReceiver.java
@@ -0,0 +1,4 @@
+package com.kokonut.NCNC.MyPage;
+
+public class MonAlarmReceiver {
+}
