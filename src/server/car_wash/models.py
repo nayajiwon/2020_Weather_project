256521e2c6ad01a7543b368a91824ca98154ce3f
below0 <mutex@kakao.com>
2020-09-06 16:08:05 +0900

    add review score

--- src/server/car_wash/models.py
+++ src/server/car_wash/models.py
@@ -34,8 +34,9 @@ class Review(models.Model):
             )
     user = models.ForeignKey(User, on_delete=models.CASCADE,
             )
+    score = models.SmallIntegerField(default=3)
     created_date = models.DateTimeField(default=timezone.now)
-    content = models.CharField(max_length=100, default = '내용이 없습니다.')
+    content = models.CharField(max_length=500, default = '내용이 없습니다.')
 
     def __str__(self):
         return self.user
--- src/server/car_wash/views.py
+++ src/server/car_wash/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.core import serializers
+from django.db.models import Avg
 
 from .forms import PostForm
 from .models import *
@@ -34,10 +35,11 @@ def washer_list(request):
 def washer_detail(request, pk):
     washer = Washer.objects.get(id=pk)
     wash_type = list(washer.wash_type.all().values())
-
+    score = round(washer.reviews.all().aggregate(Avg('score'))['score__avg'],1)
     content = {
             "name": washer.name,
-            "type": wash_type
+            "type": wash_type,
+            "score": score
             }
     return JsonResponse(content, safe=False, json_dumps_params={'ensure_ascii': False})
 
@@ -60,5 +62,6 @@ def review(request, pk):
         user_id = request.POST['id']
         user = User.objects.get(id = user_id)
         content = request.POST['content']
-        review = Review.objects.create(washer = washer, user = user, content = content)
+        score = request.POST['score']
+        review = Review.objects.create(washer = washer, user = user, content = content, score = score)
         return JsonResponse({"status":"200", "id":review.id}, safe=False, json_dumps_params={'ensure_ascii': False})
